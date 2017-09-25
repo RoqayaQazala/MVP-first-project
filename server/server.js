756f284f31238e95e20635d90c82a6bb6c998777
Roqaya Qazala <roukamohammad@gmail.com>
2017-09-25 17:08:40 +0300

    new files

--- server/server.js
+++ server/server.js
@@ -1,2 +1,13 @@
 var express= require('express');
 var app= express();
+var mongoose = require('mongoose');
+
+app.use(express.static(_dirname +'/../public'))
+app.get('/', function(res, req){
+	res.sendFile(_dirname+'/index.html')
+})
+
+var port=8000;
+
+
+module.exports = app;
